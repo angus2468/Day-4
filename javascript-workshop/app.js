5c1f7037097776ee834f41573c0a56246ba132e9
Angus <angus.baker2468@gmail.com>
2025-01-09 11:21:30 +0000

    I have finished the moodle tasks and am now commiting it

---
+++ javascript-workshop/app.js
@@ -0,0 +1,44 @@
+const myName = "Angus";
+
+console.log(myName);
+
+let age = 30;
+console.log(age);
+
+let theWeather = "Freezing";
+let myLocation = "Leicester";
+
+let myAgeDogYears = age * 7;
+
+console.log("The weather today is " + theWeather + " in " + myLocation);
+
+// Or you could od it like this
+
+console.log(`The weather today is ${theWeather} in ${myLocation}`);
+
+let firstName = "Joseph";
+console.log(`${firstName} is ${firstName.length} characters long`);
+
+let isTrue = true;
+console.log(isTrue);
+
+let isFalse = false;
+console.log(isFalse);
+
+let yourName = "Jakub";
+console.log(typeof yourName);
+
+let number = 1;
+console.log("The type of number is " + typeof number);
+
+console.log(`The type of isTrue is ${typeof isTrue}`);
+
+let budget = 50;
+let subscription = 15;
+let isbudgetGreaterThansubscription = budget > subscription;
+let isbudgetLessThansubscription = budget < subscription;
+let isbudgetEqualTosubscription = budget === subscription;
+
+console.log(isbudgetGreaterThansubscription);
+console.log(isbudgetLessThansubscription);
+console.log(isbudgetEqualTosubscription);
